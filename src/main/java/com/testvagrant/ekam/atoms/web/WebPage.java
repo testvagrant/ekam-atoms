007be0778a36a142e063e51afd6a2a78a8e5ce1a
Sudarshan <sudarshan@testvagrant.com>
2021-08-18 17:44:55 +0530

    Added MobileSCreen and WebPage

---
+++ src/main/java/com/testvagrant/ekam/atoms/web/WebPage.java
@@ -0,0 +1,35 @@
+package com.testvagrant.ekam.atoms.web;
+
+import com.google.inject.Inject;
+import com.testvagrant.ekam.atoms.MultiPlatformFinder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class WebPage extends QueryFunctions {
+
+  @Inject protected BrowserDriver browserDriver;
+  @Inject protected WebDriver driver;
+
+  protected Element element(By locator) {
+    return new Element(driver, locator);
+  }
+
+  protected Textbox textbox(By locator) {
+    return new Textbox(driver, locator);
+  }
+
+  protected Dropdown dropdown(By locator) {
+    return new Dropdown(driver, locator);
+  }
+
+  protected MultiPlatformFinder finder(By findBy, By responsiveFindBy) {
+    return MultiPlatformFinder.builder()
+        .webFindBy(findBy)
+        .responsiveFindBy(responsiveFindBy)
+        .build();
+  }
+
+  protected ElementCollection elementCollection(By locator) {
+    return new ElementCollection(driver, locator);
+  }
+}
